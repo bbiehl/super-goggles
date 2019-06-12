62b537634abeb0d0e7d8b4b7493e4439f096060c
Bradley Biehl <bradley.biehl@gmail.com>
2019-06-11 22:19:52 -0500

    slimmed down beers controller

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,51 +1,64 @@
-class BeersController < ApplicationController
-  before_action :set_beer, only: [:show, :update, :destroy]
-
+# class BeersController < ApplicationController
+class BeersController < ApiController
   # GET /beers
   def index
-    @beers = Beer.all
-
-    render json: @beers
+    @beers = Beer.select("id, name").all
+    render json: @beers.to_json
   end
 
-  # GET /beers/1
+ # GET /beers/:id
   def show
-    render json: @beer
+    @beer = Beer.find(params[:id])
+    render json: @beer.to_json
   end
-
-  # POST /beers
-  def create
-    @beer = Beer.new(beer_params)
-
-    if @beer.save
-      render json: @beer, status: :created, location: @beer
-    else
-      render json: @beer.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /beers/1
-  def update
-    if @beer.update(beer_params)
-      render json: @beer
-    else
-      render json: @beer.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /beers/1
-  def destroy
-    @beer.destroy
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_beer
-      @beer = Beer.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def beer_params
-      params.require(:beer).permit(:name, :ibu, :abv, :description)
-    end
 end
+
+  # before_action :set_beer, only: [:show, :update, :destroy]
+
+  # # GET /beers
+  # def index
+  #   @beers = Beer.all
+
+  #   render json: @beers
+
+  # # GET /beers/1
+  # def show
+  #   render json: @beer
+  # end
+
+  # # POST /beers
+  # def create
+  #   @beer = Beer.new(beer_params)
+
+  #   if @beer.save
+  #     render json: @beer, status: :created, location: @beer
+  #   else
+  #     render json: @beer.errors, status: :unprocessable_entity
+  #   end
+  # end
+
+  # # PATCH/PUT /beers/1
+  # def update
+  #   if @beer.update(beer_params)
+  #     render json: @beer
+  #   else
+  #     render json: @beer.errors, status: :unprocessable_entity
+  #   end
+  # end
+
+  # # DELETE /beers/1
+  # def destroy
+  #   @beer.destroy
+  # end
+
+  # private
+  #   # Use callbacks to share common setup or constraints between actions.
+  #   def set_beer
+  #     @beer = Beer.find(params[:id])
+  #   end
+
+  #   # Only allow a trusted parameter "white list" through.
+  #   def beer_params
+  #     params.require(:beer).permit(:name, :ibu, :abv, :description)
+  #   end
+# end
